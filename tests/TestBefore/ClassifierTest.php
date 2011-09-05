706128cee46761a77e241ff4fae83b2d0dddaf2a
Wodor Wodorski <wodor@wodor.net>
2011-09-06 01:13:51 +0200

    calculateFactors

--- tests/TestBefore/ClassifierTest.php
+++ tests/TestBefore/ClassifierTest.php
@@ -32,6 +32,17 @@ class ClassifierTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($expected, $getFactors->invoke($classifier));
     }
 
+    public function testFactorsFor100() {
+        $expected = $this->getFactorsInExpectedFormat(1, 100, 2, 50, 4, 25, 5, 20, 10);
+
+        $classifier = new Classifier(6);
+
+        $getFactors = new ReflectionMethod('\TestBefore\Classifier', 'getFactors');
+        $getFactors->setAccessible(true);
+
+        $this->assertEquals($expected, $getFactors->invoke($classifier));
+    }
+
     public function testAddFactor() {
 
         $classifier = $this->getMockBuilder('\TestBefore\Classifier')
@@ -48,10 +59,16 @@ class ClassifierTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(array(2=>2,77=>77), $this->getFactorsMethod()->invoke($classifier));
     }
 
+    private getFactorsInExpectedFormat($array) {
+       return array_combine($array,$array);
+
+    }
+
     private function getFactorsMethod() {
         $getFactors = new ReflectionMethod('\TestBefore\Classifier', 'getFactors');
         $getFactors->setAccessible(true);
         return $getFactors;
     }
+
 
 }
