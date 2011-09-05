df6a8ecfeeb61bdf4034eb7d9a2898f49e48b5d1
Wodor Wodorski <wodor@wodor.net>
2011-09-05 22:41:22 +0200

    rewrite testów

--- TestBefore/Classifier.php
+++ TestBefore/Classifier.php
@@ -3,6 +3,7 @@ namespace TestBefore;
 
 class Classifier {
 
+
     public static function isFactor($factor, $number) {
         return $number % $factor == 0;
     }
--- tests/TestBefore/ClassifierTest.php
+++ tests/TestBefore/ClassifierTest.php
@@ -5,14 +5,14 @@ class ClassifierTest extends PHPUnit_Framework_TestCase {
 
 
     public function testIs1aFactorOf10() {
-        $this->assertTrue(Classifier::isFactor(1,10));
+        $classifier = new Classifier(10);
+        $this->assertTrue($classifier->isFactor(1,10));
     }
 
     public function testFactorsFor() {
         $expected = array(1);
-        $this->assertSame($expected, Classifier::factorsFor(1));
+        $classifier = new Classifier(1);
+        $this->assertSame($expected, $classifier->getFactors(1));
     }
-
-
 
 }
